fc6b351ea17ad2310e283c641ef1e91576e8c854
lolmageap <ekxk1234@naver.com>
2024-10-02 23:58:08 +0900

    add kotest and easy random dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,8 +26,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.h2database:h2")
+    testImplementation("org.jeasy:easy-random-core:5.0.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
