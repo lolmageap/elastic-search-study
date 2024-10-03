7c8722ab1b6a511f1979e6fb3c638d7f7d0e9b36
lolmageap <ekxk1234@naver.com>
2024-10-03 19:19:34 +0900

    create test case

--- src/test/kotlin/com/example/elasticsearchstudy/OnlyRdbmsTest.kt
+++ src/test/kotlin/com/example/elasticsearchstudy/OnlyRdbmsTest.kt
@@ -1,18 +1,64 @@
 package com.example.elasticsearchstudy
 
 import io.kotest.core.spec.style.StringSpec
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.jdbc.core.JdbcTemplate
+import java.util.stream.IntStream.range
+import kotlin.system.measureTimeMillis
+import kotlin.time.measureTimedValue
 
 @SpringBootTest
 class OnlyRdbmsTest(
-    private val phoneRepository: PhoneRepository,
-): StringSpec({
+    @Autowired private val jdbcTemplate: JdbcTemplate,
+    @Autowired private val phoneRepository: PhoneRepository,
+) : StringSpec({
     beforeEach {
-        phoneRepository.deleteAllInBatch()
-        TODO("phoneRepository.saveAll()")
+        measureTimeMillis {
+            jdbcTemplate.execute("DROP TABLE IF EXISTS phone")
+            jdbcTemplate.execute(
+                """
+                CREATE TABLE phone (
+                    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                    number VARCHAR(255) NOT NULL,
+                    author VARCHAR(255) NOT NULL
+                )
+            """.trimIndent(),
+            )
+        }.apply {
+            println("Table creation time: $this ms")
+        }
+
+        val easyRandom = PhoneFactory.create()
+
+        val posts =
+            measureTimedValue {
+                range(0, 1_000_000)
+                    .parallel()
+                    .mapToObj { easyRandom.nextObject(Phone::class.java) }
+                    .toList()
+            }.apply {
+                println("Posts generation time: $duration ms")
+            }.value
+
+        measureTimeMillis {
+            jdbcTemplate.batchUpdate(
+                """
+                INSERT INTO phone (number, author)
+                VALUES (?, ?)
+            """.trimIndent(),
+                posts.map { arrayOf(it.number, it.author) },
+            )
+        }.apply {
+            println("Insertion time: $this ms")
+        }
     }
 
-    "오로지 RDBMS 만 사용해서 데이터를 조회하고 성능을 측정한다." {
-
+    "오로지 RDBMS 만 사용해서 like 연산으로 데이터를 조회하고 성능을 측정한다." {
+        measureTimeMillis {
+            phoneRepository.findAllByNumberContainingIgnoreCase("hello")
+        }.apply {
+            println("Query time: $this ms")
+        }
     }
 })
--- src/test/kotlin/com/example/elasticsearchstudy/UseElasticSearchTest.kt
+++ src/test/kotlin/com/example/elasticsearchstudy/UseElasticSearchTest.kt
@@ -1,22 +1,58 @@
 package com.example.elasticsearchstudy
 
 import io.kotest.core.spec.style.StringSpec
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.jdbc.core.JdbcTemplate
+import java.util.stream.IntStream
+import kotlin.system.measureTimeMillis
+import kotlin.time.measureTimedValue
 
 @SpringBootTest
 class UseElasticSearchTest(
+    @Autowired private val jdbcTemplate: JdbcTemplate,
     private val phoneService: PhoneService,
-    private val phoneRepository: PhoneRepository,
-    private val searchPhoneRepository: SearchPhoneRepository,
-): StringSpec({
+) : StringSpec({
+    beforeEach {
+        measureTimeMillis {
+            jdbcTemplate.execute("DROP TABLE IF EXISTS phone")
+            jdbcTemplate.execute(
+                """
+                CREATE TABLE phone (
+                    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                    number VARCHAR(255) NOT NULL,
+                    author VARCHAR(255) NOT NULL
+                )
+            """.trimIndent(),
+            )
+        }.apply {
+            println("Table creation time: $this ms")
+        }
 
-    beforeTest {
-        phoneRepository.deleteAllInBatch()
-        searchPhoneRepository.deleteAll()
-        TODO("phoneService.saveAll()")
+        val easyRandom = PhoneFactory.create()
+
+        val posts =
+            measureTimedValue {
+                IntStream.range(0, 1_000_000)
+                    .parallel()
+                    .mapToObj { easyRandom.nextObject(Phone::class.java) }
+                    .toList()
+            }.apply {
+                println("Posts generation time: $duration ms")
+            }.value
+
+        measureTimeMillis {
+            phoneService.saveAll(posts)
+        }.apply {
+            println("Insertion time: $this ms")
+        }
     }
 
     "엘라스틱 서치를 사용해서 데이터를 조회하고 성능을 측정한다." {
-
+        measureTimeMillis {
+            phoneService.findAllBy("hello")
+        }.apply {
+            println("Search time: $this ms")
+        }
     }
 })
