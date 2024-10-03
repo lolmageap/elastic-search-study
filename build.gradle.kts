d9e1c68c3cb2b5131165b8c3216eaed9ad383435
lolmageap <ekxk1234@naver.com>
2024-10-03 19:16:20 +0900

    setting jdbc and dbms

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,17 +20,20 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    runtimeOnly("com.h2database:h2")
+
     testImplementation("org.jeasy:easy-random-core:5.0.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
     testImplementation("io.kotest:kotest-assertions-core:5.7.2")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+    runtimeOnly("com.h2database:h2")
+    testRuntimeOnly("com.h2database:h2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
